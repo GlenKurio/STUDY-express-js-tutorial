63571f1341a1ab9e2a424b1f26e5ff9f5b9cb452
Oleh Minko <gleb.kurip@gmail.com>
2024-07-28 11:09:05 -0500

    cookies

--- src/index.mjs
+++ src/index.mjs
@@ -2,17 +2,21 @@ import express from "express";
 
 import { loggingMiddleware } from "../utils/middlewares.mjs";
 import routes from "./routes/index.mjs";
-
+import cookieParser from "cookie-parser";
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser("holaworlda"));
+
 app.use(routes);
+
 // app.use(loggingMiddleware); // global middleware
 
 const PORT = process.env.PORT || 3000;
 
 app.get("/", loggingMiddleware, (req, res) => {
   // local middleware
+  res.cookie("hi", "ole", { maxAge: 60000 * 60 * 2, signed: true });
   res.status(200).send("<h1>Hello World</h1>");
 });
 
--- src/routes/products.mjs
+++ src/routes/products.mjs
@@ -2,23 +2,20 @@ import { Router } from "express";
 
 const router = Router();
 router.get("/api/products", (req, res) => {
-  res.send([
-    {
-      id: 1,
-      name: "Product 1",
-      price: 100,
-    },
-    {
-      id: 2,
-      name: "Product 2",
-      price: 200,
-    },
-    {
-      id: 3,
-      name: "Product 3",
-      price: 300,
-    },
-  ]);
+  // console.log(req.headers.cookie);
+  // console.log(req.cookies);
+  console.log(req.signedCookies);
+  if (req.signedCookies.hi && req.signedCookies.hi === "ole") {
+    return res.send([
+      {
+        id: 1,
+        name: "Product 1",
+        price: 100,
+      },
+    ]);
+  }
+
+  return res.send("Not authorized");
 });
 
 export default router;
